44d9b063f69d6a23b5774cb435c111f36220b17d
Mariano Bonansea <marianobonanseapetrovial@gmail.com>
2025-04-18 19:05:28 -0300

    demo sigcatch.c

---
+++ Ejercicio1.md
@@ -0,0 +1,3 @@
+## 1. ¿Cuáles son las señales que no es posible atrapar? ¿Por qué?
+
+The signals SIGKILL and SIGSTOP cannot be caught, blocked, or ignored.
--- sigcatch.c
+++ sigcatch.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 
-int main(int argc, char *argv[])
+void manejador_senial(int signo)
 {
+    printf("Señal recibida: %s\n", strsignal(signo));
+}
+
+int main()
+{
+    // modificar el manejador de las señales
+    for (int signo = 0; signo < _NSIG; signo++)
+    {
+        sigaction(signo, manejador_senial, NULL);
+    }
+
+    // esperar señales
+    while (1)
+    {
+        pause();
+    }
+
     exit(EXIT_SUCCESS);
 }
